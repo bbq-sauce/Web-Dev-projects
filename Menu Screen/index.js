1d10ff926b538d72d687a3d29a4518a063765044
Fiza <fizasameer88@gmail.com>
2022-11-29 20:17:33 +0530

    javascript done

---
+++ Menu Screen/index.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('button');
+const navigation = document.querySelector('nav');
+const sections = document.querySelectorAll('main section');
+
+menu.addEventListener('click', ()=>{
+    navigation.classList.toggle('nav-active');
+    sections.forEach((i)=>{
+
+        i.style.display = 'block';
+        i.classList.toggle('rotate-active');
+        i.classList.toggle('closed');
+    })
+})
+
+const strokeTop =document.querySelector('.top');
+const strokeMiddle =document.querySelector('.middle');
+const strokeBottom =document.querySelector('.bottom');
+
+menu.addEventListener('click',function(){
+    strokeTop.classList.toggle('top-active');
+    strokeMiddle.classList.toggle('middle-active');
+    strokeBottom.classList.toggle('bottom-active');
+    
+})
